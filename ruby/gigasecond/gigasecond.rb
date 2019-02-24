a2125ffa57a9584079e2be1921d8ad58337799d1
Daniel Griego <eggomygriego@gmail.com>
2019-02-23 18:12:24 -0800

    add gigasecond exercise

---
+++ ruby/gigasecond/gigasecond.rb
@@ -0,0 +1,15 @@
+# a time is passed in
+
+# the problem to solve is to accept an argument
+
+# which would be a Time object
+
+# then to calculate at what point in the future
+
+class Gigasecond
+  def self.from(time)
+    gigasecond = 10 ** 9
+
+    time + gigasecond
+  end
+end
