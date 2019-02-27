3aa4451407a347ef43b901671d4934137516a78f
Daniel Griego <eggomygriego@gmail.com>
2019-02-26 21:28:27 -0800

    update acronym exercise

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,7 +1,21 @@
+require 'benchmark'
+
 class Acronym
+  # scan
+  #  u                s                 t                   r
+  #  0.000030   0.000004   0.000034 (  0.000024)
+  #  20r was the average
   def self.abbreviate(phrase)
-    phrase.split(/\W/).map do |word|
-      word[0] unless word.empty?
-    end.join.upcase
+    phrase.scan(/\b\w/).join.upcase
+  end
+
+  # split
+  #  u                s                 t                   r
+  #  0.000020   0.000003   0.000023 (  0.000019)
+  # 22r was the average
+  def self.abbreviate(phrase)
+    phrase.split(/\W/).map { |word| word[0] }.join.upcase
   end
 end
+
+p Acronym.abbreviate("bring your own beer")
